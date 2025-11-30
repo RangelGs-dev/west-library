afd11d4755f74e4b4ac430359789349031284760
Rangel Sousa <rangelgs17@gmail.com>
2025-11-30 20:45:42 -0300

    feat: adição de um contador de visitas utilizando o atributo session da request

--- catalog/views.py
+++ catalog/views.py
@@ -10,11 +10,16 @@ def index(request):
 
     number_authors = Author.objects.count()
 
+    number_visits = request.session.get('number_visits', 0)
+    number_visits += 1
+    request.session['number_visits'] = number_visits
+
     context = {
         'number_books': number_books,
         'number_instances': number_instances,
         'number_instances_available': number_instances_available,
         'number_authors': number_authors,
+        'number_visits': number_visits,
     }
 
     return render(request, 'index.html', context=context)
