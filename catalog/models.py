694058ea0154c2a29d3a43c114734c32ec032266
Rangel Sousa <rangelgs17@gmail.com>
2025-12-01 21:38:06 -0300

    feat: adição de nova permissão ao objeto BookInstance

--- catalog/models.py
+++ catalog/models.py
@@ -64,6 +64,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (('can_mark_returned', 'Seta devolução do livro'),)
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
