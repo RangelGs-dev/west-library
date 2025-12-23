87278cc0d30bbe4084b3c31b200539b0e09c5bad
Rangel Sousa <rangelgs17@gmail.com>
2025-12-22 21:19:41 -0300

    feat: incluir script para adicionar superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,26 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
+    echo "Verificando superusuário..."
+
+    python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "${DJANGO_SUPERUSER_USERNAME}"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email="${DJANGO_SUPERUSER_EMAIL}",
+        password="${DJANGO_SUPERUSER_PASSWORD}"
+    )
+    print("Superusuário criado.")
+else:
+    print("Superusuário já existe, pulando.")
+END
+else
+    echo "CREATE_SUPERUSER=false, pulando criação."
+fi
